4921a8abb097a72f667be4a9154da474d3770d04
Unknown <chatajazzy@interia.pl>
2018-05-12 19:37:08 +0200

    Add provider to allow redux and extension for chrome

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import { loadCourses } from './actions/courseActions';
+
+const store = new configureStore(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch(loadCourses());
 
 const Root = () => {
   return (
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   );
 };
 
